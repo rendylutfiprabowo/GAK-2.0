0704fbb8a98620c4cd888315a9aba645833afee4
RendyLutfiPrabowo <81194811+rendylutfiprabowo@users.noreply.github.com>
2023-06-09 09:34:21 +0700

    update role siswa

--- app/Views/_siswa/biodata.php
+++ app/Views/_siswa/biodata.php
@@ -1,6 +1,20 @@
 <?= $this->extend('_siswa/templatesiswa'); ?>
 <?= $this->section('content'); ?>
 
+<style>
+    .ahay {
+        font-weight: bold;
+        font-size: large;
+        color: #f01c64;
+       text-transform: uppercase;
+    }
+
+    .ihiy {
+        margin-top: 5px;
+        font-size: small;
+    }
+</style>
+
 <div class="container-fluid py-4">
     <div class="row">
         <div class="col-12">
@@ -9,49 +23,94 @@
 
                     <div class="container-fluid">
                         <!-- Page Heading -->
-                        <h1 class="mt-3">BIODATA DIRI</h1>
-                        <p style="border-bottom: 2px solid gray;">Mohon isi biodata berikut sesuai dengan data diri anda! Terima Kasih!</p>
-                        <form action="">
-                            <div class="input-group input-group-outline my-3">
-                                <label class="form-label">Email address</label>
-                                <input type="email" class="form-control">
+                        <h1 class="mt-3" >BIODATA DIRI</h1>
+                        <p style="border-bottom: 2px solid gray;">Mohon <b>$username siswa</b> isi biodata berikut sesuai dengan data diri anda! Terima Kasih!</p>
+                        <form action="#">
+                            <div class="row">
+                                <div class="col-lg-6 col-md-6 my2">
+                                    <div class="form-group my-2">
+                                        <div class="input-group input-group-static mb-4">
+                                            <label>Email address</label>
+                                            <input type="email" class="form-control ahay ahay" required value="asda@gmail">
+                                            <p class="ihiy"><b>Petunjuk : </b></p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 my2">
+                                    <div class="form-group my-2">
+                                        <div class="input-group input-group-static mb-4">
+                                            <label>Nama Lengkap</label>
+                                            <input type="text" class="form-control ahay" required value="Rendy Lutfi">
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="input-group input-group-outline my-3">
-                                <label class="form-label">Nama Lengkap</label>
-                                <input type="text" class="form-control">
+                            <div class="row">
+                                <div class="col-lg-6 col-md-6 my2">
+                                    <div class="form-group my-2">
+                                        <div class="input-group input-group-static mb-4">
+                                            <label>Nomor Kartu PKH</label>
+                                            <input type="text" class="form-control ahay" required value="08096785875">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 my2">
+                                    <div class="form-group my-2">
+                                        <div class="input-group input-group-static mb-4">
+                                            <label>Nama Pendamping PKH</label>
+                                            <input type="text" class="form-control ahay" required value="Yanto Yanti">
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="input-group input-group-outline my-3">
-                                <label class="form-label">No PKH</label>
-                                <input type="text" class="form-control">
+                            <div class="row">
+                                <div class="col-lg-6 col-md-6 my2">
+                                    <div class="form-group my-2">
+                                        <div class="input-group input-group-static mb-4">
+                                            <label for="exampleFormControlSelect1" class="ms-0">Asal Wilayah</label>
+                                            <select class="form-control ahay" required value="tester value" id="exampleFormControlSelect1">
+                                                <option>Bandarlampung</option>
+                                                <option>Pdasdasd</option>
+                                                <option>sad</option>
+                                                <option>asd</option>
+                                                <option>asdasd</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 my2">
+                                    <div class="form-group my-2">
+                                        <div class="input-group input-group-static mb-4">
+                                            <label>Asal Sekolah</label>
+                                            <input type="text" class="form-control ahay" required value="SMA 1 BANDARLAMPUNG">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-6 col-md-6 my2">
+                                    <div class="form-group my-2">
+                                        <div class="input-group input-group-static mb-4">
+                                            <label>Nama Ibu</label>
+                                            <input type="text" class="form-control ahay" required value="tester value">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 my2">
+                                    <div class="form-group my-2">
+                                        <div class="input-group input-group-static mb-4">
+                                            <label>No WhatsApp Siswa</label>
+                                            <input type="text" class="form-control ahay" required value="tester value">
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
 
-                            <div class="input-group input-group-static mb-4">
-                                <label for="exampleFormControlSelect1" class="ms-0">Asal Wilayah</label>
-                                <select class="form-control" id="exampleFormControlSelect1">
-                                    <option>1</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>4</option>
-                                    <option>5</option>
-                                </select>
-                            </div>
-                            <div class="input-group input-group-outline my-3">
-                                <label class="form-label">Asal Sekolah</label>
-                                <input type="text" class="form-control">
-                            </div>
-                            <div class="input-group input-group-outline my-3">
-                                <label class="form-label">Nama Ibu</label>
-                                <input type="text" class="form-control">
-                            </div>
-                            <div class="input-group input-group-outline my-3">
-                                <label class="form-label">Nama Pendamping PKH</label>
-                                <input type="text" class="form-control">
-                            </div>
-                            
-                            <div class="input-group input-group-outline my-3">
-                                <label class="form-label">No WhatsApp</label>
-                                <input type="text" class="form-control">
-                            </div>
+
+
+
+
+
 
                             <div class="d-grid gap-2 d-md-flex justify-content-md-end">
                                 <button class="btn btn-primary me-md-2" type="button">Simpan dan Lanjutkan</button>
@@ -59,6 +118,7 @@
                         </form>
 
                     </div>
+
                 </div>
             </div>
         </div>
--- app/Views/_siswa/datauniv.php
+++ app/Views/_siswa/datauniv.php
@@ -1,6 +1,21 @@
 <?= $this->extend('_siswa/templatesiswa'); ?>
 <?= $this->section('content'); ?>
 
+<style>
+    .ahay {
+        font-weight: bold;
+        font-size: large;
+        color: #f01c64;
+        text-transform: uppercase;
+    }
+
+    .ihiy {
+        margin-top: 5px;
+        font-size: small;
+    }
+</style>
+
+
 <div class="container-fluid py-4">
     <div class="row">
         <div class="col-12">
@@ -12,23 +27,34 @@
                         <h1 class="mt-3">DATA UNIVERSITAS</h1>
                         <p style="border-bottom: 2px solid gray;">Jika anda <b>sudah lulus tes masuk kuliah, mohon isi </b>form berikut sesuai dengan data universitas anda!. <b>Jika belum mohon abaikan</b>! Terima Kasih!</p>
                         <form action="">
-                            <div class="input-group input-group-outline my-3">
-                                <label class="form-label">Nama Perguruan Tinggi</label>
-                                <input type="email" class="form-control">
+                            <div class="form-group my-2">
+                                <div class="input-group input-group-static mb-4">
+                                    <label>Nama Perguruan Tinggi</label>
+                                    <input type="email" class="form-control ahay " required value="Harvard University">
+                                    <p class="ihiy"><b>contoh : </b>UNIVERSITAS LAMPUNG, UNIVERSITAS GAJAH MADA</p>
+                                </div>
                             </div>
-
-                            <div class="input-group input-group-outline my-3">
-                                <label class="form-label">Program Studi</label>
-                                <input type="text" class="form-control">
+                            <div class="form-group my-2">
+                                <div class="input-group input-group-static mb-4">
+                                    <label>Program Studi</label>
+                                    <input type="email" class="form-control ahay " required value="S1 ILMU KOMPUTER">
+                                    <p class="ihiy"><b>contoh : </b>S1 ILMU KOMPUTER, D3 MANAJEMEN INFORMATIKA</p>
+                                </div>
                             </div>
+                           
                             <div class="input-group input-group-static mb-4">
                                 <label for="exampleFormControlSelect1" class="ms-0">Tahun Masuk</label>
                                 <select class="form-control" id="exampleFormControlSelect1">
-                                    <option>1</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>4</option>
-                                    <option>5</option>
+                                    <option>2015</option>
+                                    <option>2016</option>
+                                    <option>2017</option>
+                                    <option>2018</option>
+                                    <option>2019</option>
+                                    <option>2020</option>
+                                    <option>2021</option>
+                                    <option>2022</option>
+                                    <option>2023</option>
+                                    <option>2024</option>4
                                 </select>
                             </div>
                             <div class="d-grid gap-2 d-md-flex justify-content-md-end">
