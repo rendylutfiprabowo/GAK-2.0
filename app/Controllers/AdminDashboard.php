de0a55d894c7265e30fb130d027d43811f9179cf
RendyLutfiPrabowo <81194811+rendylutfiprabowo@users.noreply.github.com>
2023-06-22 12:48:00 +0700

    Update AdminDashboard.php

--- app/Controllers/AdminDashboard.php
+++ app/Controllers/AdminDashboard.php
@@ -19,38 +19,44 @@ class AdminDashboard extends BaseController
 
     public function AdminDashboard()
     {
-        $user = new UserModel();
-        $biodata = new Biodata();
-        $universitas = new Universitas();
-        $prestasi = new Prestasi();
-        $upload = new Upload();
+        if (session()->get('user') == '0') {
+            $user = new UserModel();
+            $biodata = new Biodata();
+            $universitas = new Universitas();
+            $prestasi = new Prestasi();
+            $upload = new Upload();
 
-        // jumlah siswa
-        $jumlah_siswa = $user->countAllResults();
+            // jumlah siswa
+            $jumlah_siswa = $user->countAllResults();
 
-        // jumlah biodata
-        $jumlah_biodata = $biodata->countAllResults();
+            // jumlah biodata
+            $jumlah_biodata = $biodata->countAllResults();
 
-        // jumlah universitas
-        $jumlah_universitas = $universitas->countAllResults();
+            // jumlah universitas
+            $jumlah_universitas = $universitas->countAllResults();
 
-        // jumlah prestasi
-        $jumlah_prestasi = $prestasi->countAllResults();
+            // jumlah prestasi
+            $jumlah_prestasi = $prestasi->countAllResults();
 
-        // jumlah upload
-        $jumlah_upload = $upload->countAllResults();
+            // jumlah upload
+            $jumlah_upload = $upload->countAllResults();
 
-        $mappingkab = $biodata->getAsal_wilayah();
+            $mappingkab = $biodata->getAsal_wilayah();
 
 
-        $data = [
-            'mappingKab' => $mappingkab,
-            'jumlahSiswa' => $jumlah_siswa,
-            'jumlahBiodata' => $jumlah_biodata,
-            'jumlahUniversitas' => $jumlah_universitas,
-            'jumlahPrestasi' => $jumlah_prestasi,
-            'jumlahUpload' => $jumlah_upload,
-        ];
-        return view('_admin/dashboard', $data);
+            $data = [
+                'mappingKab' => $mappingkab,
+                'jumlahSiswa' => $jumlah_siswa,
+                'jumlahBiodata' => $jumlah_biodata,
+                'jumlahUniversitas' => $jumlah_universitas,
+                'jumlahPrestasi' => $jumlah_prestasi,
+                'jumlahUpload' => $jumlah_upload,
+            ];
+            return view('_admin/dashboard', $data);
+        } elseif (session()->get('user') == '1') {
+            return redirect()->to(base_url('Biodata'));
+        } else {
+            return redirect()->to(base_url('login'));
+        }
     }
 }
