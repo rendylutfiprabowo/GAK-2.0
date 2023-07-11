e39666b27dfbdc2d4cb575ba580f5bb47f018e61
RendyLutfiPrabowo <81194811+rendylutfiprabowo@users.noreply.github.com>
2023-07-11 12:51:23 +0700

    Update editadmin.php

--- app/Views/_admin/editadmin.php
+++ app/Views/_admin/editadmin.php
@@ -19,84 +19,101 @@
             </div>
             <div class="card-body px-0 mx-4 ">
                 <div class="m-0 p-0">
-                    <div class="row mt-5">
+                    
+                        <button type="submit" class="btn btn-primary mt-5" value="submit">Simpan Data</button>
+                        <a href="/PKHLolosPTN/detail/<?= $PTN['id_siswa'] ?>"><button type="button" class="btn bg-gradient-secondary mt-5 ms-2">Batal</button></a>
+                    
+                    <div class="row mt-3">
                         <div class="col-lg-6 col-md-6 my2">
-                            <div class="input-group input-group-outline my-3">
+                            <div class="form-group my-2">
+                                <label for="email_address">Email</label>
                                 <input placeholder="Email" name="email_address" id="email_address" type="email_address" class="form-control" value="<?= $PTN['email_address'] ?>">
                             </div>
                         </div>
                         <div class=" col-lg-6 col-md-6 my2">
                             <div class="form-group my-2">
-                                <div class="input-group input-group-outline my-3">
-                                    <input placeholder="Nama Siswa" name="nama" id="nama" type="text" class="form-control" readonly value="<?= $PTN['nama'] ?>">
-                                </div>
+                                <label for="nama">Nama Lengkap</label>
+                                <input placeholder="Nama Siswa" name="nama" id="nama" type="text" class="form-control" readonly value="<?= $PTN['nama'] ?>">
                             </div>
                         </div>
                     </div>
-                    <div class="row mt-3">
-                        <div class="col-lg-6 col-md-6 my2">
-                            <div class="input-group input-group-outline my-3">
-                                <input placeholder="Nomor Telepon Seluler" name="no_whatshap" id="no_whatshap" type="tel" class="form-control" value="<?= $PTN['no_whatshap'] ?>">
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-md-6 my2">
-                            <div class="input-group input-group-outline my-3">
-                                <input placeholder="Provinsi" name="provinsi" id="provinsi" type="text" class="form-control" value="<?= $PTN['provinsi'] ?>">
-                            </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-lg-6 col-md-6 my2">
+                        <div class="form-group my-2">
+                            <label for="no_whatshap">Nomor WhatsApp</label>
+                            <input placeholder="Nomor Telepon Seluler" name="no_whatshap" id="no_whatshap" type="tel" class="form-control" value="<?= $PTN['no_whatshap'] ?>">
                         </div>
                     </div>
-                    <div class="row mt-3">
-                        <div class="col-lg-6 col-md-6 my2">
-                            <div class="input-group input-group-outline my-3">
-                                <input placeholder="Kabupaten / Kota" name="kabupaten_kota" id="kabupaten_kota" type="text" class="form-control" id="kabupaten_kota" value="<?= $PTN['kabupaten_kota'] ?>">
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-md-6 my2">
-                            <div class="input-group input-group-outline my-3">
-                                <input placeholder="Kecamatan" type="text" id="kecamatan" name="kecamatan" class="form-control" readonly value="<?= $PTN['kecamatan'] ?>">
-                            </div>
+                    <div class="col-lg-6 col-md-6 my2">
+                        <div class="form-group my-2">
+                            <label for="provinsi">Provinsi</label>
+                            <input placeholder="Provinsi" name="provinsi" id="provinsi" type="text" class="form-control" value="<?= $PTN['provinsi'] ?>">
                         </div>
                     </div>
-                    <div class="row mt-3">
-                        <div class="col-lg-6 col-md-6 my2">
-                            <div class="input-group input-group-outline my-3">
-                                <input placeholder="Desa / Kelurahan" type="text" id="desa" name="desa" class="form-control" readonly value="<?= $PTN['desa'] ?>">
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-md-6 my2">
-                            <div class="input-group input-group-outline my-3">
-                                <input placeholder="Asal Sekolah" type="text" id="asal_sekolah" name="asal_sekolah" class="form-control" readonly value="<?= $PTN['asal_sekolah'] ?>">
-                            </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-lg-6 col-md-6 my2">
+                        <div class="form-group my-2">
+                            <label for="kabupaten_kota">Kabupaten / Kota</label>
+                            <input placeholder="Kabupaten / Kota" name="kabupaten_kota" id="kabupaten_kota" type="text" class="form-control" id="kabupaten_kota" value="<?= $PTN['kabupaten_kota'] ?>">
                         </div>
                     </div>
-                    <div class="row mt-3">
-                        <div class="col-lg-6 col-md-6 my2">
-                            <div class="input-group input-group-outline my-3">
-                                <input placeholder="Nomor PKH" type="text" name="no_pkh" class="form-control" id="no_pkh" readonly value="<?= $PTN['no_pkh'] ?>">
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-md-6 my2">
-                            <div class="input-group input-group-outline my-3">
-                                <input placeholder="Jalur Masuk PTN" type="text" name="jalur_masukptn" class="form-control" id="jalur_masukptn" value="<?= $PTN['jalur_masukptn'] ?>">
-                            </div>
+                    <div class="col-lg-6 col-md-6 my2">
+                        <div class="form-group my-2">
+                            <label for="kecamatan">Kecamatan</label>
+                            <input placeholder="Kecamatan" type="text" id="kecamatan" name="kecamatan" class="form-control" readonly value="<?= $PTN['kecamatan'] ?>">
                         </div>
                     </div>
-                    <div class="row mt-3">
-                        <div class="col-lg-6 col-md-6 my2">
-                            <div class="input-group input-group-outline my-3">
-                                <input placeholder="Universitas" type="text" name="universitas" class="form-control" id="universitas" value="<?= $PTN['universitas'] ?>">
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-md-6 my2">
-                            <div class="input-group input-group-outline my-3">
-                                <input placeholder="Program Studi" type="text" name="program_studi" class="form-control" id="program_studi" value="<?= $PTN['program_studi'] ?>">
-                            </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-lg-6 col-md-6 my2">
+                        <div class="form-group my-2">
+                            <label for="desa">Desa / Kelurahan</label>
+                            <input placeholder="Desa / Kelurahan" type="text" id="desa" name="desa" class="form-control" readonly value="<?= $PTN['desa'] ?>">
                         </div>
                     </div>
-                    <div class="row mt-3">
-                        <div class="col-lg-6 col-md-6 my2">
+                    <div class="col-lg-6 col-md-6 my2">
+                        <div class="form-group my-2">
+                            <label for="asal_sekolah">Asal Sekolah</label>
+                            <input placeholder="Asal Sekolah" type="text" id="asal_sekolah" name="asal_sekolah" class="form-control" readonly value="<?= $PTN['asal_sekolah'] ?>">
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-lg-6 col-md-6 my2">
+                        <div class="form-group my-2">
+                            <label for="no_pkh">Nomor PKH</label>
+                            <input placeholder="Nomor PKH" type="text" name="no_pkh" class="form-control" id="no_pkh" readonly value="<?= $PTN['no_pkh'] ?>">
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 my2">
+                        <div class="form-group my-2">
+                            <label for="jalur_masukptn">Jalur Masuk PTN</label>
+                            <input placeholder="Jalur Masuk PTN" type="text" name="jalur_masukptn" class="form-control" id="jalur_masukptn" value="<?= $PTN['jalur_masukptn'] ?>">
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-lg-6 col-md-6 my2">
+                        <div class="form-group my-2">
+                            <label for="universitas">Universitas</label>
+                            <input placeholder="Universitas" type="text" name="universitas" class="form-control" id="universitas" value="<?= $PTN['universitas'] ?>">
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 my2">
+                        <div class="form-group my-2">
+                            <label for="program_studi">Program Studi</label>
+                            <input placeholder="Program Studi" type="text" name="program_studi" class="form-control" id="program_studi" value="<?= $PTN['program_studi'] ?>">
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-lg-6 col-md-6 my2">
+                        <div class="form-group my-2">
 
                             <div class="form-group my-2">
+                                <label for="program_studi">Status KIP K</label>
                                 <select placeholder="Status KIP K" name="status_kip" class="form-select" aria-label="Default select example">
                                     <option value="xxxxxxx" selected>"<?= $PTN['status_kip'] ?>"</option>
 
@@ -104,12 +121,10 @@
                             </div>
                         </div>
                     </div>
-
-                    <button type="submit" class="btn btn-primary mt-2" value="submit">Simpan Data</button>
-                    <a href="/PKHLolosPTN/detail/<?= $PTN['id_siswa'] ?>"><button type="button" class="btn bg-gradient-secondary mt-2 ms-2">Batal</button></a>
                 </div>
             </div>
         </div>
+    </div>
     </div>
 </form>
 <?= $this->endSection() ?>
